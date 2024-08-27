2373e8a587319e83c269a686a0ab8c8e25f95f5c
YousufNaveedKhan <yk033127@gmail.com>
2024-08-27 11:29:04 +0500

    PHPCourse-24

---
+++ Lecture01-Introduction/Introduction.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture01: Introduction to PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h1>My first PHP program!</h1>";
+
+    ?>
+</body>
+</html>
---
+++ Lecture02-VariablesAndPrintStatement/VariablesAndPrintStatement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture02: Variables and Print Statement</title>
+</head>
+<body>
+    <?php
+        $color = "red";
+
+        EcHo "<h1>Hello World2!</h1>";
+        ECHO "<h1>Hello World2!</h1>";
+        echo "My favourite color is " . $color;
+        echo "My car is " . $CoLoR;
+        echo "My car is " . $COLOR;
+    ?>
+</body>
+</html>
